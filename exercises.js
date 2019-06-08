80a194f532c0eecade8a71a193a9b47a1bacf3cd
iharri0601 <40372695+iharri0601@users.noreply.github.com>
2019-06-08 08:02:23 -0400

    initial commit

---
+++ exercises.js
@@ -0,0 +1,11 @@
+var message = "Welcome to JS";
+console.log("Welcome to JS");
+console.log("Welcome to JS");
+console.log("Welcome to JS");
+console.log("Welcome to JS");
+
+var firstNumber=50;
+var secondNumber=100;
+var thirdNumber=500;
+var sum = firstNumber + secondNumber + thirdNumber;
+console.log(sum);
---
+++ main.js
@@ -0,0 +1,39 @@
+var message = "Welcome to JS";
+console.log("Welcome to JS");
+console.log("Welcome to JS");
+console.log("Welcome to JS");
+console.log("Welcome to JS");
+
+var firstNumber = 50;
+var secondNumber = 100;
+var thirdNumber = 500;
+var sum = firstNumber + secondNumber + thirdNumber;
+console.log(sum);
+
+var sentence1 = "Welcome to JavaScript.";
+var oneword = "JavaScript is one word.";
+var learningjs = "I'm excited to learn JavaScript!";
+var keyword = "var is a JavaScript keyword, along with several other words.";
+
+var name = "Izaiah";
+console.log("Izaiah");
+console.log("Izaiah");
+console.log("Izaiah");
+
+console.log("Hello, World");
+console.log("Hey, ya'll!");
+console.log("Whoops we forgot something");
+console.log("Peanut butter and jelly sandwich");
+console.log("Open and close using the same kind of quote");
+
+console.log(10 + 12 * 31 - 4 / 4);
+
+var car = "my broken car";
+var carVerb = "broke down";
+var driverVerb = "I fixed";
+var past = "yesterday";
+var present = "today";
+
+console.log("my broken car" + "broke down" + "yesterday");
+console.log("today" + "my broken car" + "broke down");
+console.log("I fixed" + "my broken car" + "today");
